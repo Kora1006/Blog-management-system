0e4901787b9aa51600980c4896cce35c8e4e40cb
Kora1006 <471209058@qq.com>
2019-09-05 19:29:22 +0800

    03-完成前台及后台的页面渲染

--- controllers/pageController.js
+++ controllers/pageController.js
@@ -1,4 +1,5 @@
 module.exports = {
+    // 渲染前台用户页面
     getIndexPage: (req, res) => {
         res.render('index.ejs')
     },
@@ -8,7 +9,42 @@ module.exports = {
     getDetailPage: (req, res) => {
         res.render('detail.ejs')
     },
+    // 渲染后台管理页面
     getLoginPage: (req, res) => {
-
+        res.render('admin/login.ejs')
+    },
+    getAdminIndexPage: (req, res) => {
+        res.render('admin/index.ejs')
+    },
+    getAdminCategoriesPage:(req,res)=>{
+        res.render('admin/categories.ejs')
+    },
+    getAdminCommentsPage:(req,res)=>{
+        res.render('admin/comments.ejs')
+    },
+    getAdminNavMenusPage:(req,res)=>{
+        res.render('admin/nav-menus.ejs')
+    },
+    getAdminPasswordResetPage:(req,res)=>{
+        res.render('admin/password-reset.ejs')
+    },
+    getAdminPostAddPage:(req,res)=>{
+        res.render('admin/post-add.ejs')
+    },
+    getAdminPostsPage:(req,res)=>{
+        res.render('admin/posts.ejs')
+    },
+    getAdminProfilePage:(req,res)=>{
+        res.render('admin/profile.ejs')
+    },
+    getAdminSettingPage:(req,res)=>{
+        res.render('admin/settings.ejs')
+    },
+    getAdminSlidesPage:(req,res)=>{
+        res.render('admin/slides.ejs')
+    },
+    getAdminUsersPage:(req,res)=>{
+        res.render('admin/users.ejs')
     }
+
 }
--- router.js
+++ router.js
@@ -7,9 +7,18 @@ router.get('/', pageController.getIndexPage)  //渲染主页
 .get('/list', pageController.getListPage)  //渲染列表页
 .get('/detail', pageController.getDetailPage)  //渲染详细页
  // 渲染后台主页
- .get('/admin', (req, res) => {
-        res.render('admin/index.ejs')
-    })
+ .get('/admin',pageController.getAdminIndexPage)
+ .get('/admin/login',pageController.getLoginPage)
+ .get('/admin/categories',pageController.getAdminCategoriesPage)
+ .get('/admin/comments',pageController.getAdminCommentsPage)
+ .get('/admin/nav-menus',pageController.getAdminNavMenusPage)
+ .get('/admin/password-reset',pageController.getAdminPasswordResetPage)
+ .get('/admin/post-add',pageController.getAdminPostAddPage)
+ .get('/admin/posts',pageController.getAdminPostsPage)
+ .get('/admin/profile',pageController.getAdminProfilePage)
+ .get('/admin/setting',pageController.getAdminSettingPage)
+ .get('/admin/slides',pageController.getAdminSlidesPage)
+ .get('/admin/users',pageController.getAdminUsersPage)
 
 // 暴露
 module.exports = router
