f4c0823f873d41ff847638801dc8c9585cb33491
Kora1006 <471209058@qq.com>
2019-09-10 20:30:01 +0800

    20-完成分类页单选和复选框的操作逻辑

--- assets/js/admin_categories.js
+++ assets/js/admin_categories.js
@@ -1,6 +1,7 @@
 
 
 $(function () {
+    // ajax发送请求获取页面分类数据
     $.ajax({
         type: 'get',
         url: '/getCateData',
@@ -13,5 +14,40 @@ $(function () {
                 $('tbody').html(htmlStr)
             }
         }
+    })
+
+    // 管理复选框
+    // 点击全选框时选择所有分类的复选框并显示批量删除
+    $('#checkAll').on('change', function () {
+        let checkStatus = $(this).prop('checked')  //判断当前全选框的状态
+        if (checkStatus) {
+            $('tbody .checkSingle').prop('checked', true)
+            $('.btn-sm').show()
+        } else {
+            $('tbody .checkSingle').prop('checked', false)
+            $('.btn-sm').hide()
+        }
+        let checkNum = $('tbody .checkSingle:checked').length
+        if(checkNum >1){
+            $('.btn-sm').show()
+        }else{
+            $('.btn-sm').hide()
+        }
+    })
+
+    // 给单个的复选框注册点击事件（事件委托）
+    $('tbody').on('click', '.checkSingle', function () {
+        let checkNum = $('tbody .checkSingle:checked').length
+        if(checkNum >1){
+            $('.btn-sm').show()
+        }else{
+            $('.btn-sm').hide()
+        }
+       let checkBoxNum = $('tbody .checkSingle').length
+       if(checkBoxNum == checkNum){
+           $('#checkAll').prop('checked',true)
+       }else{
+        $('#checkAll').prop('checked',false)
+       }
     })
 })
